789814bca8a315824639a48800d10b9d9245dc3a
Kre8er <Sparkyaaron@gmail.com>
2024-04-11 20:27:34 -0500

    Added touchscreen test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,90 @@
-#include <Arduino.h>
+// Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+// Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+// Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+// Note the following larger fonts are primarily numeric only!
+// Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+// Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+// Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+
+#define XPT2046_IRQ 36
+#define XPT2046_MOSI 32
+#define XPT2046_MISO 39
+#define XPT2046_CLK 25
+#define XPT2046_CS 33
 
 // put function declarations here:
-int myFunction(int, int);
+
+SPIClass mySpi = SPIClass(VSPI);
+XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
+TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("ILI9341 Test!");
+
+  // Start the SPI for the touch screen and init the TS library
+  mySpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  ts.begin(mySpi);
+  ts.setRotation(1);
+
+  // Start the tft display and set it to black
+  tft.init();
+  tft.setRotation(1); //This is the display in landscape
+
+  // Clear the screen before writing to it
+  tft.fillScreen(TFT_BLACK);
+
+  int x = 320 / 2; // center of display
+  int y = 100;
+  int fontSize = 2;
+  tft.drawCentreString("Touch Screen to Start", x, y, fontSize);
 }
 
+void printTouchToSerial(TS_Point p) {
+  Serial.print("Pressure = ");
+  Serial.print(p.z);
+  Serial.print(", x = ");
+  Serial.print(p.x);
+  Serial.print(", y = ");
+  Serial.print(p.y);
+  Serial.println();
+}
+
+void printTouchToDisplay(TS_Point p) {
+
+  // Clear screen first
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  int x = 320 / 2; // center of display
+  int y = 100;
+  int fontSize = 2;
+
+  String temp = "Pressure = " + String(p.z);
+  tft.drawCentreString(temp, x, y, fontSize);
+
+  y += 16;
+  temp = "X = " + String(p.x);
+  tft.drawCentreString(temp, x, y, fontSize);
+
+  y += 16;
+  temp = "Y = " + String(p.y);
+  tft.drawCentreString(temp, x, y, fontSize);
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (ts.tirqTouched() && ts.touched()) {
+    TS_Point p = ts.getPoint();
+    printTouchToSerial(p);
+    printTouchToDisplay(p);
+    delay(100);
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
